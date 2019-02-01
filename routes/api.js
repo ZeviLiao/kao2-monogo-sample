12af92844f9f44d83a2be7f7f8cd52763deca9c0
Zevi Liao <liao.zevi@nuwarobotics.com>
2019-02-01 17:23:57 +0800

    mutiple insert.

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
-const router = require('koa-router')()
+const router = require("koa-router")()
 const mongoose = require("mongoose")
 
-router.prefix('/api')
+router.prefix("/api")
 
 // router.get('/test', function (ctx, next) {
 //   ctx.body = 'test!'
@@ -11,24 +11,62 @@ const db = mongoose.connect("mongodb://localhost/testDB")
 
 // 账户的数据库模型
 var UserSchema = new mongoose.Schema({
-    username:String,
-    password:String,
-    email:String
+	a: {
+		type: 'String'
+	},
+	b: {
+		type: 'Number'
+	},
+	c: {
+		type: 'Boolean'
+	},
+	d: {
+		type: 'Number'
+	}
 });
 var User = mongoose.model('User',UserSchema);
 
 // 新增数据
-var user = {
-  username: 'ydj',
-  password: '123123',
-  email: ''
-}
-var newUser = new User(user);
-newUser.save();
+var user = [{
+    "a": "2",
+    "b": 2,
+    "c": true,
+    "d": 3.4
+  },
+  {
+    "a": "3",
+    "b": 2,
+    "c": true,
+    "d": 3.4
+  },
+  {
+    "a": "4",
+    "b": 2,
+    "c": true,
+    "d": 3.4
+  },
+  {
+    "a": "5",
+    "b": 2,
+    "c": true,
+    "d": 3.4
+  },
+]
+// var newUser = new User(user);
+// newUser.save();
+User.create(user, function (err, jellybean, snickers) {
+  if (err) {
+      console.log(err)
+  }
+  else{
+      console.log('done')
+  }
+});
 
 router.get('/test', async (ctx, next) => {
 	let val = null
-	const data = await User.findOne({username: 'ydj'})
+    // const data = await User.remove({})
+    const data = {}
 	console.log('data', data)
 	const result = {
 		code:200,
@@ -38,5 +76,6 @@ router.get('/test', async (ctx, next) => {
 	ctx.response.body = result
 	return result
 })
+
 
 module.exports = router
